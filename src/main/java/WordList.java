843384c83358fc2ef3d11222e7072a73798ebaad
wispo12 <143028172+wispo12@users.noreply.github.com>
2024-04-17 10:42:36 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -8,15 +8,34 @@ public class WordList
   public void add(String word) { myList.add(word); }
   public String toString() { return myList.toString(); }
 
-  public int numWordsOfLength(int len)
-  {
-    /* to be implemented in part (a) */
-    
-  }
+  public int numWordsOfLength(int length)
+{
+    int numWords = 0;
 
-  public void removeWordsOfLength(int len)
-  {
-    /* to be implemented in part (b) */
+    for(int i = 0; i < myList.size(); i++)
+    {
+        String word = (String) myList.get(i);
+        
+        if(word.length() == length)
+            numWords++;
+    }
 
-  }
+    return numWords;
+}
+
+  public void removeWordsOfLength(int length)
+{
+    int i = 0;
+
+    while(i < myList.size())
+    {
+        String word = (String) myList.get(i);
+        
+        if(word.length() == length)
+            myList.remove(i);
+        else
+            i++;
+    }
+}
+  
 }
